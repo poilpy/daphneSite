f0f8456aed5787f860492a9ee740ab9c4ec7b4be
Colin Murphy <colinmurphy97@gmail.com>
2024-11-14 10:29:44 -0800

    Hopefully done

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,20 @@
 import { defineConfig } from 'astro/config';
 
 import tailwind from '@astrojs/tailwind';
+import swup from '@swup/astro';
+
+import alpinejs from '@astrojs/alpinejs';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [
+    tailwind(), 
+    swup({
+    // @ts-ignore
+    theme: 'fade'
+    
+
+    }), 
+    alpinejs(),
+  ]
 });
